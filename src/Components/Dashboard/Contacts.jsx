1a05216b92923bf0d7e89b35944f6f48106f05bc
Abhideep Maity <abhideepm@gmail.com>
2020-07-26 23:04:26 +0530

    added contacts display

--- src/Components/Dashboard/Contacts.jsx
+++ src/Components/Dashboard/Contacts.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
 
-const Contacts = () => {
-	return <div></div>
+const Contacts = ({ contactsData }) => {
+	return (
+		<div className="mx-auto w-75 mt-5">
+			<div className="card shadow my-5">
+				<div className="card-body text-center">
+					<h3 className="card-title">Contacts</h3>
+					<table className="table">
+						<thead className="thead-dark">
+							<tr>
+								<th scope="col">Name</th>
+								<th scope="col">Email</th>
+								<th scope="col">Phone</th>
+							</tr>
+						</thead>
+						<tbody>
+							{contactsData.map(item => (
+								<tr>
+									<td>{item.name}</td>
+									<td>{item.email}</td>
+									<td>{item.phone}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Contacts
