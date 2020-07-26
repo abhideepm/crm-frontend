12460321ccd6bf02512a01a332cae6aa8c53b86b
Abhideep Maity <abhideepm@gmail.com>
2020-07-26 23:55:09 +0530

    added delete functionality

--- src/Components/Dashboard/Leads.jsx
+++ src/Components/Dashboard/Leads.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import axios from 'axios'
 
 const Leads = ({ leadsData }) => {
+	const token = localStorage.getItem('token')
+	const deleteData = async id => {
+		try {
+			const res = await axios.delete(
+				`https://limitless-badlands-01612.herokuapp.com/leads/${id}`,
+				{
+					headers: { 'Content-Type': 'application/json', authenticate: token },
+				}
+			)
+			if (res.data === 'Forbidden') alert('Access Denied')
+		} catch (err) {
+			console.log(err)
+		}
+	}
+
 	return (
 		<div className="mx-auto w-75 mt-5">
 			<div className="card shadow my-5 animate__bounceIn">
 				<div className="card-body text-center">
 					<div className="text-right">
 						<button className="btn btn-success">
-							<i class="fas fa-plus"></i>
+							<i className="fas fa-plus"></i>
 						</button>
 					</div>
 					<h3 className="card-title">Leads</h3>
@@ -30,10 +46,13 @@ const Leads = ({ leadsData }) => {
 									<td>{item.status}</td>
 									<td className="d-flex justify-content-around">
 										<button className="btn btn-primary">
-											<i class="fas fa-edit"></i>
+											<i className="fas fa-edit"></i>
 										</button>
-										<button className="btn btn-danger">
-											<i class="fas fa-trash"></i>
+										<button
+											className="btn btn-danger"
+											onClick={() => deleteData(item._id)}
+										>
+											<i className="fas fa-trash"></i>
 										</button>
 									</td>
 								</tr>
--- src/Components/Dashboard/Requests.jsx
+++ src/Components/Dashboard/Requests.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import axios from 'axios'
 
 const Requests = ({ requestsData }) => {
+	const token = localStorage.getItem('token')
+	const deleteData = async id => {
+		try {
+			const res = await axios.delete(
+				`https://limitless-badlands-01612.herokuapp.com/requests/${id}`,
+				{
+					headers: { 'Content-Type': 'application/json', authenticate: token },
+				}
+			)
+			if (res.data === 'Forbidden') alert('Access Denied')
+		} catch (err) {
+			console.log(err)
+		}
+	}
+
 	return (
 		<div className="mx-auto w-75 mt-5">
 			<div className="card shadow my-5 animate__bounceIn">
 				<div className="card-body text-center">
 					<div className="text-right">
 						<button className="btn btn-success">
-							<i class="fas fa-plus"></i>
+							<i className="fas fa-plus"></i>
 						</button>
 					</div>
 					<h3 className="card-title">Service Requests</h3>
@@ -28,10 +44,13 @@ const Requests = ({ requestsData }) => {
 									<td>{item.status}</td>
 									<td className="d-flex justify-content-around">
 										<button className="btn btn-primary">
-											<i class="fas fa-edit"></i>
+											<i className="fas fa-edit"></i>
 										</button>
-										<button className="btn btn-danger">
-											<i class="fas fa-trash"></i>
+										<button
+											className="btn btn-danger"
+											onClick={() => deleteData(item._id)}
+										>
+											<i className="fas fa-trash"></i>
 										</button>
 									</td>
 								</tr>
