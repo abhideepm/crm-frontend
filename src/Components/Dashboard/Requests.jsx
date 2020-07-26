10e6eed980fc39cc7eb19b324a02a0742224a00c
Abhideep Maity <abhideepm@gmail.com>
2020-07-26 23:12:33 +0530

    added service requests display

--- src/Components/Dashboard/Requests.jsx
+++ src/Components/Dashboard/Requests.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
 
-const Requests = () => {
-	return <div></div>
+const Requests = ({ requestsData }) => {
+	return (
+		<div className="mx-auto w-75 mt-5">
+			<div className="card shadow my-5">
+				<div className="card-body text-center">
+					<h3 className="card-title">Service Requests</h3>
+					<table className="table">
+						<thead className="thead-dark">
+							<tr>
+								<th scope="col">Title</th>
+								<th scope="col">Requested By</th>
+								<th scope="col">Status</th>
+							</tr>
+						</thead>
+						<tbody>
+							{requestsData.map(item => (
+								<tr key={item._id}>
+									<td>{item.title}</td>
+									<td>{item.contact}</td>
+									<td>{item.status}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Requests
